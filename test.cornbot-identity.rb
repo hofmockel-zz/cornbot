ec0738aab19090f55de67b82a5ebdf4a77abf4c1
Ryan Frahm <frahmryan@gmail.com>
2014-12-12 11:55:53 -0600

    Update test.cornbot-identity.rb

--- test.cornbot-identity.rb
+++ test.cornbot-identity.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 # Cornbot private identity file
 
-IDENTITY = 'bigbadcornbot'
+IDENTITY = 'testcornbot'
 PASSWORD = 'ilovedrupalcorn'
 
 FORECASTIOAPIKEY = 'c2899416c6678bb44053d94cf480dd23'
